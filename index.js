cf7d4cdbf713903189fd810dfb33f67044f3e3b3
Pascual Requena Lama <reque1993@gmail.com>
2018-03-19 10:53:16 +0000

    Gpi-stats added. Close #12

--- index.js
+++ index.js
@@ -13,12 +13,185 @@ var BASE_API_PATH = "/api/v1";
 
 var dbAgr = __dirname+"/f-one-drivers.db";
 var dbMotoGPChampions = __dirname+"/motogpchampions.db";
+var dbGpiStats = __dirname+"/gpi-stats.db";
 
 
 app.use("/",express.static(path.join(__dirname+"/public")));
 app.get("/hello",(req,res)=>{
     res.send("Hello World");
 });
+
+//////////////////////////////////////PRL (GPI-STATS)///////////////////////////////////////
+var gpi_stats;
+var ini_gpi_stats = [
+                 { "country": "iceland",
+                   "year": 2013,
+                   "score": 1162,
+                   "rank": 1,
+                   "population" : 323764
+                  },
+                  { "country": "portugal",
+                   "year": 2016,
+                   "score": 1356,
+                   "rank": 5,
+                   "population" : 10371627
+                  },{ "country": "spain",
+                   "year": 2017,
+                   "score": 1568,
+                   "rank": 25,
+                   "population" : 46354321
+                  },{ "country": "syria",
+                   "year": 2015,
+                   "score": 3645,
+                   "rank": 162,
+                   "population" : 18734987
+                  },{ "country": "japan",
+                   "year": 2017,
+                   "score": 1408,
+                   "rank": 10,
+                   "population" : 127484450
+                  }]
+//Data Store//                  
+var dbGpi = new DataStore({
+    
+    filename: dbGpiStats,
+    autoload: true
+    
+});
+
+//Fill the DB//
+app.get(BASE_API_PATH+"/gpi-stats/loadInitialData",(req,res)=>{
+    console.log(Date()+" - Trying to load Stats");
+    
+dbGpi.find({},(err,stats)=>{
+    if(err){
+        console.error("error accesing db");
+        process.exit(1);
+    }
+    if(stats.length == 0){
+        console.log("empty db");
+        dbGpi.insert(ini_gpi_stats);
+    }else{
+        console.log("DB initiallited with "+stats.length+"stats");
+    }
+});
+    
+});
+
+//Methods//
+
+//GET base path//
+app.get(BASE_API_PATH+"/gpi-stats",(req,res)=>{
+    console.log(Date() + " - GET / gpi-stats");
+    
+    dbGpi.find({},(err,stats)=>{
+    if(err){
+        console.error("Error accesing DB");
+        res.sendStatus(500);
+        return;
+    }
+        
+    res.send(stats);
+    });
+
+});
+
+//GET to a resource//
+
+app.get(BASE_API_PATH+"/gpi-stats/:year",(req,res)=>{
+    var year = req.params.year;
+    
+    console.log(Date() + " - GET / gpi-stats/" + year);
+    dbGpi.find({year : parseInt(year)},(err,stat)=>{
+    if(err){
+        console.error("Error acceso DB");
+        res.sendStatus(500);
+        return;
+    }
+    res.send(stat);
+    });
+    
+});
+
+//GET to a resource using 2 parameters//
+
+app.get(BASE_API_PATH+"/gpi-stats/:country/:year/",(req,res)=>{
+    var country = req.params.country;
+    var year = req.params.year;
+    
+    console.log(Date() + " - GET / gpi-stats/" +country + "/"+year);
+    dbGpi.find({country: country, year : parseInt(year)},(err,stat)=>{
+        if(err){
+           console.error("Error acceso DB");
+            res.sendStatus(500);
+         return;  
+        }
+        res.send(stat);
+    });
+});
+
+//POST to base path//
+
+app.post(BASE_API_PATH+"/gpi-stats",(req,res)=>{
+    console.log(Date() + " - POST / gpi-stats");
+    var stat = req.body;
+    dbGpi.insert(stat);
+    //f_one_drivers.push(driver);
+    res.sendStatus(201); //Created
+});
+
+//POST to a resource//
+
+app.post(BASE_API_PATH+"/gpi-stats/:year",(req,res)=>{
+    var year = req.params.year;
+    
+    console.log(Date() + " - POST / f-one-drivers/" + year +" - Hacking attempt detected");
+    res.sendStatus(405); //Method Not Allowed
+});
+
+//PUT base path//
+
+app.put(BASE_API_PATH+"/gpi-stats",(req,res)=>{
+    console.log(Date() + " - PUT / gpi-stats - Hacking attempt detected");
+    res.sendStatus(405);//Method Not Allowed
+});
+
+//PUT a un recurso
+app.put(BASE_API_PATH+"/gpi-stats/:year",(req,res)=>{
+    var year = req.params.year;
+    var stat = req.body;
+    
+    console.log(Date() + " - PUT / gpi-stats/" + year);
+        if(year != stat.year){
+        res.sendStatus(409);//Conflict
+        console.warn(Date() + " - Hacking attempt!");
+        return;
+    }
+    
+    dbGpi.update({year : parseInt(stat.year)},stat,(err,numUpdate)=>{console.log("Updated: " + numUpdate);});
+    res.sendStatus(200);//OK
+});
+
+//DELETE base path//
+app.delete(BASE_API_PATH+"/gpi-stats",(req,res)=>{
+    console.log(Date() + " - DELETE / gpi-stats");
+    gpi_stats = [];
+    
+    dbGpi.remove({},{multi:true});
+    
+    res.sendStatus(200);//OK
+});
+
+//DELETE resource//
+app.delete(BASE_API_PATH+"/gpi-stats/:year",(req,res)=>{
+    var year = req.params.year;
+    
+    console.log(Date() + " - DELETE / gpi-stats/" + year);
+    dbGpi.remove({year : parseInt(year)});
+    res.sendStatus(200);//OK
+});
+
+
 ////////////////AGR/////////////////////////////////////////////////////////////////////////
 
 var f_one_drivers;
@@ -155,7 +328,7 @@ app.put(BASE_API_PATH+"/f-one-drivers/:year",(req,res)=>{
         return;
     }
     
-    dbF_One.update({year : parseInt(driver.year)},driver,(err,numUpdate)=>{console.log("Updated: " + numUpdate)});
+    dbF_One.update({year : parseInt(driver.year)},driver,(err,numUpdate)=>{console.log("Updated: " + numUpdate);});
     
 
     /*f_one_drivers = f_one_drivers.map((c)=>{
